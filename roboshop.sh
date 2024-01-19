110f411a0fa145a157136e441a091e301f51e274
chandu141186 <chandu141186@gmail.com>
2024-01-19 21:22:31 +0530

    robhofina

--- roboshop.sh
+++ roboshop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
+SG_ID=sg-0fdb2e1836e4f66c3 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
+ZONE_ID=Z06286322TWN251SZBYW9 # replace your zone ID
 DOMAIN_NAME="chandulearn.online"
 
 for i in "${INSTANCES[@]}"
